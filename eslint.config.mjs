53560938fc2d1c2a8a20dda91cc0560ba16c3104
Network Based Solutions, LLC <35822984+NBS-LLC@users.noreply.github.com>
2024-04-21 00:55:28 -0700

    chore: vscode extensions (#4)
    * chore: vscode eslint ext config
    
    * chore: vscode eslint ext config
    
    * chore: recommended vscode exts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,4 +9,20 @@ export default tseslint.config(
   {
     ignores: ["webpack.config.js"],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
 );
