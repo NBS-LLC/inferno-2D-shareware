164a04982e92e3672e7c096513ec2497c789dab5
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-04-22 14:55:40 -0700

    wip: unit test the player class

--- src/Player.spec.ts
+++ src/Player.spec.ts
@@ -1,12 +1,15 @@
+import { expect } from "chai";
+import { GameObjects, Physics } from "phaser";
+import { instance, mock } from "ts-mockito";
 import { Player } from "./Player";
 
 describe(Player.name, () => {
   describe(Player.prototype.faceLeft.name, () => {
-    it.skip("should face the player left", () => {
-      // const mockShape = mock(GameObjects.Shape);
-      // const mockBody = mock(Physics.Arcade.Body);
-      // const player = new Player(instance(mockShape), instance(mockBody));
-      // expect(player).to.be.instanceOf(Player);
+    it("should face the player left", () => {
+      const mockShape = mock(GameObjects.Shape);
+      const mockBody = mock(Physics.Arcade.Body);
+      const player = new Player(instance(mockShape), instance(mockBody));
+      expect(player).to.be.instanceOf(Player);
     });
   });
 });
