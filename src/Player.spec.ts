dd1199c04e5b1ba68a36ff0ec304e0933338529f
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-04-21 21:11:32 -0700

    wip: unit test player class

---
+++ src/Player.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { Player } from "./Player";
+
+describe(Player.name, () => {
+  describe(Player.prototype.faceLeft.name, () => {
+    it("should face the player left", () => {
+      expect(false).to.be.true;
+    });
+  });
+});
