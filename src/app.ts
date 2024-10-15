dec02604f8a648ff771d14e9729bed8dbea59e13
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-10-15 16:24:19 -0700

    wip - basic player object using matter physics

--- src/app.ts
+++ src/app.ts
@@ -1,124 +1,47 @@
 import { Game, Scene } from "phaser";
-import { Enemy } from "./Enemy";
-import { Player } from "./Player";
 
-import GameConfig = Phaser.Types.Core.GameConfig;
-import Key = Phaser.Input.Keyboard.Key;
-import Text = Phaser.GameObjects.Text;
+class MainScene extends Scene {
+  private player: Phaser.GameObjects.Polygon;
 
-const config: GameConfig = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-  },
-  scene: {
-    init,
-    preload,
-    create,
-    update,
-  },
-};
-
-const game = new Game(config);
-let debugging: boolean = false;
-let player: Player;
-let enemy: Enemy;
-const playerInput: { [key: string]: Key } = {};
-let fpsText: Text;
-let pointerText: Text;
-
-function init(this: Scene) {
-  this.input.keyboard.addKey("esc").on("down", handleRestart.bind(this));
-
-  this.input.keyboard
-    .addKey("backtick")
-    .on("down", () => (debugging = !debugging));
-}
-
-function preload(this: Scene) {}
-
-function create(this: Scene) {
-  const background = this.add.graphics();
-  const bgGradient = getDayGradient();
-  background.fillGradientStyle(
-    bgGradient[0],
-    bgGradient[1],
-    bgGradient[2],
-    bgGradient[3],
-    1,
-  );
-  background.fillRect(0, 0, 800, 600);
-
-  player = Player.createDefault(this);
-
-  enemy = Enemy.createDefault(this, 700, 400);
-  enemy.faceLeft();
-
-  playerInput["right"] = this.input.keyboard.addKey("RIGHT");
-  playerInput["left"] = this.input.keyboard.addKey("LEFT");
-  playerInput["up"] = this.input.keyboard.addKey("UP");
-  playerInput["down"] = this.input.keyboard.addKey("DOWN");
-
-  playerInput["face-left"] = this.input.keyboard.addKey("A");
-  playerInput["face-right"] = this.input.keyboard.addKey("D");
-
-  playerInput["fire-primary"] = this.input.keyboard.addKey("SPACE");
-
-  fpsText = this.add.text(16, 32, "", { fontSize: "16px", color: "#FFF" });
-  pointerText = this.add.text(16, 48, "", { fontSize: "16px", color: "#FFF" });
-}
-
-function update(this: Scene) {
-  player.stopMoving();
-
-  if (playerInput["right"].isDown) {
-    player.moveRight();
+  constructor() {
+    super("GameScene");
   }
 
-  if (playerInput["left"].isDown) {
-    player.moveLeft();
-  }
+  create() {
+    const playerVertices = [
+      [0, 0],
+      [40, 10],
+      [0, 20],
+    ];
 
-  if (playerInput["up"].isDown) {
-    player.moveUp();
-  }
+    this.player = this.add.polygon(
+      100,
+      400,
+      playerVertices,
+      Phaser.Display.Color.GetColor(110, 110, 110),
+    );
 
-  if (playerInput["down"].isDown) {
-    player.moveDown();
+    this.matter.add.gameObject(this.player);
   }
-
-  if (playerInput["face-left"].isDown) {
-    player.faceLeft();
-  }
-
-  if (playerInput["face-right"].isDown) {
-    player.faceRight();
-  }
-
-  if (playerInput["fire-primary"].isDown) {
-    player.firePrimaryWeapon();
-  }
-
-  fpsText.setText(debugging ? getFPSDetails() : "");
-  pointerText.setText(debugging ? getPointerDetails(this) : "");
 }
 
-function getDayGradient(): number[] {
-  return [0x0288d1, 0x288d1, 0xacf0f2, 0xacf0f2];
+class Inferno2DGame extends Game {
+  constructor() {
+    super({
+      type: Phaser.CANVAS,
+      parent: "game",
+      width: 800,
+      height: 600,
+      scene: [MainScene],
+      physics: {
+        default: "matter",
+        matter: {
+          debug: true,
+          gravity: { x: 0, y: 0 },
+        },
+      },
+    });
+  }
 }
 
-function getFPSDetails(): string {
-  return "FPS: " + game.loop.actualFps.toFixed(2);
-}
-
-function getPointerDetails(scene: Scene): string[] {
-  const pointer = scene.input.activePointer;
-  return ["x: " + pointer.x, "y: " + pointer.y];
-}
-
-function handleRestart(this: Scene) {
-  debugging = false;
-  this.scene.restart();
-}
+new Inferno2DGame();
